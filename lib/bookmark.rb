df97e75ac2644136bda8707e32a0b46150faa8ec
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-31 13:51:23 +0000

    Created database helper and refactored tests to use it

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -31,8 +31,8 @@ class Bookmark
       connection = PG.connect(dbname: 'bookmark_manager')
     end
 
-    connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title;")
-
+    result = connection.exec("INSERT INTO bookmarks (title, url) VALUES('#{title}', '#{url}') RETURNING id, url, title;")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
 end
--- spec/units/bookmark_spec.rb
+++ spec/units/bookmark_spec.rb
@@ -1,4 +1,5 @@
 require 'bookmark'
+require 'database_helpers'
 
 RSpec.describe Bookmark do
 
@@ -15,7 +16,7 @@ RSpec.describe Bookmark do
 
       expect(bookmarks.length).to eq 3
       expect(bookmarks.first).to be_a Bookmark
-      expect(bookmarks.first.id).to eq bookmark.first['id']
+      expect(bookmarks.first.id).to eq bookmark.id
       expect(bookmarks.first.title).to eq 'Makers Academy'
       expect(bookmarks.first.url).to eq "http://www.makersacademy.com"
     end
@@ -25,10 +26,13 @@ RSpec.describe Bookmark do
   describe '#add' do
 
     it 'adds a new bookmark' do
-      bookmark = Bookmark.add(url: 'http://google.com', title: 'Google').first
+      bookmark = Bookmark.add(url: 'http://google.com', title: 'Google')
+      persisted_data = persisted_data(id: bookmark.id)
 
-      expect(bookmark['url']).to eq 'http://google.com'
-      expect(bookmark['title']).to eq 'Google'
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data.first['id']
+      expect(bookmark.url).to eq 'http://google.com'
+      expect(bookmark.title).to eq 'Google'
     end
 
   end
